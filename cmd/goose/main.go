ac6e1d09e3def06858d94912e38b7e68a6f9fc36
Sergey Gritsay <darknezz31ru@gmail.com>
2023-12-09 21:25:01 +0300

    Adding goose file

---
+++ cmd/goose/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/pressly/goose"
+)
+
+var (
+	flags = flag.NewFlagSet("goose", flag.ExitOnError)
+	dir   = flag.String("dir", ".", "directory with migration files")
+)
+
+func main() {
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+
+	args := flag.Args()
+
+	if len(args) < 2 {
+		flags.Usage()
+		return
+	}
+
+	dbstring, command := args[1], args[2]
+
+	db, err := sql.Open("postgres", dbstring)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalf("goose: Failed to close db: %v\n", err)
+		}
+	}()
+
+	arguments := []string{}
+	if len(args) > 3 {
+		arguments = append(arguments, args[3:]...)
+	}
+
+	if err := goose.Run(command, db, *dir, arguments...); err != nil {
+		log.Fatalf("goose %v: %v", command, err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module lamoda_test_task
 go 1.21.4
 
 require github.com/gorilla/rpc v1.2.1
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pressly/goose v2.7.0+incompatible // indirect
+)
